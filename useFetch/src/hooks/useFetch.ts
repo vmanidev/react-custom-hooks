cd041125cee68837647828970a520abfc1b2b24a
vmanidev <vmanidev@outlook.com>
2025-10-02 01:26:44 +0530

    feat: implement useFetch custom hook

---
+++ useFetch/src/hooks/useFetch.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import type { ErrorPayload } from "../types/fetch";
+
+
+export default function useFetch(url: string) {
+
+    const [data, setData] = useState(null);
+    const [error, setError] = useState<ErrorPayload>(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+        fetch(url)
+            .then(async (response) => {
+                if (!response.ok) {
+                    setError({ message: "Network error." });
+                }
+                setData((await response.json()))
+            })
+            .catch(() => {
+                setData(null);
+                setError({ message: "Failed to fetch data." });
+            })
+            .finally(() => setLoading(false));
+    }, [url]);
+
+    return { data, error, loading };
+}
---
+++ useFetch/src/types/fetch.d.ts
@@ -0,0 +1,1 @@
+export type ErrorPayload = { message: string } | null;
