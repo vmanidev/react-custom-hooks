9045e267d4ed6630b9a96efbe6af1fd302aeb0dc
vmanidev <vmanidev@outlook.com>
2025-10-02 20:21:49 +0530

    feat(app): integrate useFetch hook to load and render posts

--- useFetch/src/App.tsx
+++ useFetch/src/App.tsx
@@ -1,3 +1,30 @@
+import useFetch from "./hooks/useFetch";
+
+import "./App.css";
+
 export default function App() {
-  return <div>useFetch</div>;
+  const { data, loading, error } = useFetch(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+
+  const getData = () => {
+    return data.map((item: any, index: number) => (
+      <li key={index}>
+        <div>{item.title}</div>
+        <div>{item.body}</div>
+      </li>
+    ));
+  };
+
+  return (
+    <>
+      {loading ? (
+        <div id="loading">Loading...</div>
+      ) : data ? (
+        <ol>{getData()}</ol>
+      ) : (
+        <div id="error">{error?.message}</div>
+      )}
+    </>
+  );
 }
