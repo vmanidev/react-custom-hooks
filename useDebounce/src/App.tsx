b049ed89ca20289b2d23c51f0f3e29db9b625a96
vmanidev <vmanidev@outlook.com>
2025-10-07 01:24:03 +0530

    feat: add example demonstrating usage of useDebounce custom hook

--- useDebounce/src/App.tsx
+++ useDebounce/src/App.tsx
@@ -1,3 +1,24 @@
+import { useState } from "react";
+
+import "./App.css";
+import useDebounce from "./hooks/useDebounce";
+
 export default function App() {
-  return <div>useDebounce</div>;
+  const [text, setText] = useState("");
+
+  const debounce = useDebounce(setText, 1000);
+
+  return (
+    <div id="app">
+      <input
+        type="text"
+        name="text-input"
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+          debounce(event.target.value)
+        }
+      />
+      <pre id="live-text-display">{text}</pre>
+      {text.length > 0 && <p>Note: Text will appear 1 seconds after input.</p>}
+    </div>
+  );
 }
